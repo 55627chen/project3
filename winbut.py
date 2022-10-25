5626809960815548806a2e6e8e2ba4711cb7bff5
55627chen <chen55627@gmail.com>
2022-10-25 14:38:21 +0800

    PACK

--- winbut.py
+++ winbut.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import tkinter.font as tkFont
 
 
 class Window(tk.Tk):
@@ -14,7 +15,13 @@ class Window(tk.Tk):
         mainCanvas.create_image(0, 0, anchor=tk.NW, image=self.tkImage)
         #tk.Button(mainCanvas, text="click", image=self.butImage).pack()
         mainCanvas.pack(fill=tk.BOTH, expand=True)
-        # ------建立背景
+        # END------建立背景
+        # ------建立Label
+        helv36 = tkFont.Font(family='Helvetica',
+                             size=36, weight='bold')
+        tk.Label(mainCanvas, text="職能發展學院", font=('arial', 30),
+                 background='#C9C8CD').place(x=370, y=80)
+        # END------建立Label
 
 
 def main():
