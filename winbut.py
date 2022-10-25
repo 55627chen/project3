bc8d0fffa9226a283f31f126dda8338688b0adf7
55627chen <chen55627@gmail.com>
2022-10-25 14:05:47 +0800

    reisizeable

--- winbut.py
+++ winbut.py
@@ -5,21 +5,23 @@ from PIL import Image, ImageTk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        # ----建立背景
         bgImage = Image.open('bg.jpg')
         self.tkImage = ImageTk.PhotoImage(bgImage)
         buImage = Image.open('1_.png')
         self.butImage = ImageTk.PhotoImage(buImage)
         mainCanvas = tk.Canvas(self)
         mainCanvas.create_image(0, 0, anchor=tk.NW, image=self.tkImage)
-        tk.Button(mainCanvas, text="click", image=self.butImage).pack()
-
+        #tk.Button(mainCanvas, text="click", image=self.butImage).pack()
         mainCanvas.pack(fill=tk.BOTH, expand=True)
+        # ------建立背景
 
 
 def main():
     window = Window()
     window.title("Frame框架")
-    window.geometry("640x427")
+    window.resizable(0, 0)
+    window.geometry("640x427+300+200")
     window.mainloop()
 
 
